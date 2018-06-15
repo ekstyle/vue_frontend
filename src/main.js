654339a5cbbb8f8b4fcb69f6c4874765a2fa324e
pozdnjakov <pozdnjakov@ekb.kassy.ru>
2018-06-15 13:19:09 +0500

    Add Axios

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+
+axios.get('/api/')
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
 
 new Vue({
   router,
